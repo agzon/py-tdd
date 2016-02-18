6793172f865b420a8648366d4682130d8a7f52e9
Arthur Gustavsson <argu01@gmail.com>
2016-02-18 20:46:36 +0100

    New function check_for_row_in_list_table in functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text, [row.text for row in rows])
+
   def test_can_start_a_list_and_retrieve_it_later(self):
     # user goes to check out homepage
     self.browser.get('http://localhost:8000')
@@ -33,10 +38,7 @@ class NewVisitorTest(unittest.TestCase):
     # User hits enter, the page updates and now the page lists:
     # "1: Buy Milk" as an item in a to-do list
     inputbox.send_keys(Keys.ENTER)
-
-    # TEST:
-    #import time
-    #time.sleep(10)
+    self.check_for_row_in_list_table('1: Buy milk')
 
     # There is still a text box inviting to add another item. The user 
     # enters "Buy bread"
@@ -44,10 +46,8 @@ class NewVisitorTest(unittest.TestCase):
     inputbox.send_keys('Buy bread')
     inputbox.send_keys(Keys.ENTER)
 
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy milk', [row.text for row in rows])
-    self.assertIn('2: Buy bread', [row.text for row in rows])
+    self.check_for_row_in_list_table('1: Buy milk')
+    self.check_for_row_in_list_table('2: Buy bread')
 
     self.fail('Finish the test')
 
