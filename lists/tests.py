e8e98f26587a1d0ef19880752b143ab63d8de34c
arthur2 <argu01@gmail.com>
2016-02-19 12:05:22 +0100

    Rebasing master

--- lists/tests.py
+++ lists/tests.py
@@ -30,3 +30,24 @@ class HomePageTest(TestCase):
     )
     self.assertEqual(response.content.decode(), expected_html)
 
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+
+  def test_saving_and_retrieveing_items(self):
+    first_item = Item()
+    first_item.text = 'First (ever) list item'
+    first_item.save()
+
+    second_item = Item()
+    second_item.text = 'Item the second'
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    self.assertEqual(saved_items.count(), 2)
+
+    first_Saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+
+    self.assertEqual(first_saved_item.text, first_item.text)
+    self.assertEqual(second_saved_item.text, second_item.text)
