24013bd3bd6b861ffffadf3ecf15d452e41f19d3
arthur2 <argu01@gmail.com>
2016-02-20 20:18:52 +0100

    Updated FT to test 2 users with lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertIn(row_text, [row.text for row in rows])
 
   def test_can_start_a_list_and_retrieve_it_later(self):
-    # user goes to check out homepage
+    # Edith goes to check out homepage
     self.browser.get(self.live_server_url)
 
     # She notices the page title and header mention To-Do /list
@@ -26,38 +26,60 @@ class NewVisitorTest(LiveServerTestCase):
     header_text = self.browser.find_element_by_tag_name('h1').text
     self.assertIn('To-Do', header_text)
 
-    # User is invited to enter a to-do item straight away
+    # Edith is invited to enter a to-do item straight away
     inputbox = self.browser.find_element_by_id('id_new_item')
     self.assertEqual(
       inputbox.get_attribute('placeholder'),
       'Enter a to-do item'
     )
 
-    # User types "Buy milk" into a text box
+    # Edith types "Buy milk" into a text box
     inputbox.send_keys('Buy milk')
 
-    # User hits enter, the page updates and now the page lists:
+    # Edith hits enter, the page updates and now the page lists:
     # "1: Buy Milk" as an item in a to-do list
     inputbox.send_keys(Keys.ENTER)
+    edith_list_url = self.browser.current_url
+    self.assertRegex(edith_list_url, '/lists/.+')
     self.check_for_row_in_list_table('1: Buy milk')
 
-    # There is still a text box inviting to add another item. The user 
+    # There is still a text box inviting to add another item. Edith 
     # enters "Buy bread"
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Buy bread')
     inputbox.send_keys(Keys.ENTER)
 
+    # The page updates again, and now shows both items in the list
     self.check_for_row_in_list_table('1: Buy milk')
     self.check_for_row_in_list_table('2: Buy bread')
 
-    self.fail('Finish the test')
+    # Now a new user, Francis, comes along to the site.
 
-    # The page updates again, and now shows both items in the list
+    ## We use a new browser session to make sure that no information
+    ## of Edith's is coming through from cookies etc
+    self.browser.quit()
+    self.browser = webdriver.Firefox()
 
-    # The user wondes whether the site will remember her to-do list.
-    # Then she sees the the site has generated a unique URL for her
+    # Francis visits the home page. There is no sign of Edith's list
+    self.browser.get(self.live_server_url)
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy milk', page_text)
+    self.assertNotIn('Buy bread', page_text)
 
-    # The user visits that URL - her to-do list is still there.
+    # Francis starts a new list by entering a new item.
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Go to work')
+    inputbox.send_keys(Keys.ENTER)
 
-    # Satisfied, the user leaves the web page
+    # Francis gets his own unique URL
+    francis_list_url = self.browser.current_url
+    self.assertRegex(francis_list_url, '/lists/.+')
+    self.assertNotEqual(francis_list_url, edith_list_url)
+
+    # Again, there is no trace of Edith's list
+    page_text = self.browser.find_element_by_tag_name('body').text
+    self.assertNotIn('Buy milk', page_text)
+    self.assertNotIn('Buy bread', page_text)
+
+    # Satisfied, they both go back to sleep
 
